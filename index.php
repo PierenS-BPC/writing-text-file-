0096859c1dc3ca12a17077bf330b802aa2bae1d0
Sarah Pieren <32098863+PierenS-BPC@users.noreply.github.com>
2018-01-09 14:07:52 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!";
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+
+
+$myfile = fopen("newfile.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("newfile.txt"));
+fclose($myfile);
+
+<body>
+</html>
