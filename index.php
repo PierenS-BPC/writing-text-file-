49473da65b93de6a84fdbea6eea6e884e70f346b
Sarah Pieren <32098863+PierenS-BPC@users.noreply.github.com>
2018-01-09 14:12:51 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!";
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
 $txt = "John Doe\n";
 fwrite($myfile, $txt);
 fclose($myfile);
